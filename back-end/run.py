6f6ab5890830098d876b662e5810b9ff84de494f
Chiraze Drine <chzchz96@gmail.com>
2024-03-16 17:41:49 -0400

    fix main and flask db issue

--- back-end/run.py
+++ back-end/run.py
@@ -1,14 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
+from app import app
 
-load_dotenv()
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../movies.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-from . import models
-from . import routes
+if __name__ == "__main__":
+    app.run(debug=True)
