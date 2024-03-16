2bf44ff2a33a5cfe815d7ab18776694f6998bbdb
Chiraze Drine <chzchz96@gmail.com>
2024-03-16 16:50:32 -0400

    Initial commit including project structure

---
+++ back-end/app/utils.py
@@ -0,0 +1,9 @@
+import requests
+
+def fetch_movies(api_key):
+    """Fetch movies from TMDb."""
+    # Your implementation here
+
+def fetch_genres(api_key):
+    """Fetch genres from TMDb."""
+    # Your implementation here
